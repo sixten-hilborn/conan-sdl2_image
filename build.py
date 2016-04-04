866bbcc17af73b447b8910e9d3d7147a0ea420b3
Lasote <lasote@gmail.com>
2016-04-04 12:57:08 +0200

    Exclude x86 packages

--- build.py
+++ build.py
@@ -4,4 +4,8 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="SDL2_image:shared", pure_c=True)
-    builder.run()
+    x86_64_builds = []
+    for build in builder.builds: # Problems installing native GL libs for x86
+        if not build[0]["arch"] == "x86":
+            x86_64_builds.append([build[0], build[1]])
+    builder.builds = x86_64_builds
